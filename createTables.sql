d7b3bb2ef58329e96ae758b14794996a24b96d17
OYAMA <oyamab@oregonstate.edu>
2021-05-11 15:25:03 -0700

    added rest of tables with comments...queries will be underneath I believe like the downloaded versions from class

--- createTables.sql
+++ createTables.sql
@@ -6,6 +6,12 @@ DROP TABLE IF EXISTS playerItemPurchases;
 DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS teams;
 -- Create Tables
+CREATE TABLE teams(
+    teamID int NOT NULL AUTO_INCREMENT,
+    title varchar(250) NOT NULL,
+    PRIMARY KEY teamID
+) ENGINE=InnoDB;
+
 --  Since team id cant be null we need to create teams first?
 --  Or if not created then it auto creates and adds to table for teams?
 Create Table players(
@@ -22,3 +28,34 @@ Create Table players(
     FOREIGN KEY (teamID) REFERENCES teams(teamsID)
 ) ENGINE=InnoDB;
 
+CREATE TABLE itemShops(
+    itemID int AUTO_INCREMENT NOT NULL,
+    itemName varchar(250) NOT NULL UNIQUE,
+    itemDescription varchar(250) NOT NULL,
+    vBuckCost int DEFAULT 0,
+    PRIMARY KEY itemID
+) ENGINE=InnoDB;
+
+CREATE TABLE playerItemPurchases(
+    ID int AUTO_INCREMENT,
+    playerID int NOT NULL,
+    itemID int NOT NULL,
+    PRIMARY KEY ID,
+    FOREIGN KEY (playerID) REFERENCES player(playerID),
+    FOREIGN KEY (itemID) REFERENCES itemShops(itemID)
+) ENGINE=InnoDB;
+
+CREATE TABLE playerInventories(
+    ID int AUTO_INCREMENT,
+    playerID int NOT NULL,
+    health int DEFAULT 100,
+    medKit int Default 0,
+    shield int DEFAULT 0,
+    shieldPotion int DEFAULT 0,
+    shotgun int DEFAULT 0,
+    shotgunAmmo int DEFAULT 0,
+    rifelAmmo int DEFAULT 0,
+    xpPoints int DEFAULT 0,
+    PRIMARY KEY ID,
+    FOREIGN KEY (playerID) REFERENCES player(playerID)
+) ENGINE=InnoDB;
