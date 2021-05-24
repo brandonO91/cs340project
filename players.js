125bdeb3e79f37c944023e93feb49cfc23be67ba
Stanley <johnssta@oregonstate.edu>
2021-05-24 06:10:18 -0700

    all tables now display and added create (insert)

--- players.js
+++ players.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getPlayers(res, mysql, context, complete){
-        mysql.pool.query("SELECT playerID, fname, lname, vbucks, level, teamID, playerAlive FROM players", function(error, results, fields){
+        mysql.pool.query("SELECT playerID as id, fname, lname, vbucks, level, teamID, playerAlive FROM players", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -13,8 +13,22 @@ module.exports = function(){
         });
     }
 
+    function getPlayer(res, mysql, context, id, complete){
+        console.log(id)
+        var sql = "SELECT playerID as id, fname, lname, vbucks, level, teamID, playerAlive FROM players WHERE playerID = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.getPlayer = results[0];
+            complete();
+        });
+    }
+
     function getTeams(res, mysql, context, complete){
-        mysql.pool.query("SELECT teamID, title FROM teams", function(error, results, fields){
+        mysql.pool.query("SELECT teamID as id, title FROM teams", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -27,6 +41,7 @@ module.exports = function(){
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
+        context.jsscripts = ["deleteplayer.js","filterplayers.js","searchplayers.js"];
         var mysql = req.app.get('mysql');
         getTeams(res, mysql, context, complete);
         getPlayers(res, mysql, context, complete);
@@ -57,6 +72,62 @@ module.exports = function(){
             }
         });
     });
+
+    /* Display one person for the specific purpose of updating people */
+
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["selectedteam.js", "updateplayer.js"];
+        var mysql = req.app.get('mysql');
+        getPlayer(res, mysql, context, req.params.id, complete);
+        getTeams(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('update-player', context);
+            }
+
+        }
+    });
+
+    /* The URI that update data is sent to in order to update a player */
+
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        console.log(req.body)
+        console.log(req.params.id)
+        var sql = "UPDATE players SET fname=?, lname=?, vbucks=?, level=?, teamID=?, playerAlive=? WHERE playerID=?";
+        var inserts = [req.body.fname, req.body.lname, req.body.vbucks, req.body.level, req.body.teamID, req.body.playerAlive, req.params.id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+
+    /* Route to delete a player, simply returns a 202 upon success. Ajax will handle this. */
+
+    router.delete('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM players WHERE playerID = ?";
+        var inserts = [req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
 
     return router;
 }();
--- teams.js
+++ teams.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getTeams(res, mysql, context, complete){
-        mysql.pool.query("SELECT teamID, title FROM teams", function(error, results, fields){
+        mysql.pool.query("SELECT teamID as id, title FROM teams", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -17,6 +17,7 @@ module.exports = function(){
         var callbackCount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
+        context.jsscripts = ["deleteteam.js"];
         getTeams(res, mysql, context, complete);
         function complete(){
             callbackCount++;
@@ -45,6 +46,22 @@ module.exports = function(){
             }
         });
     });
+
+    router.delete('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM teams WHERE teamID = ?";
+        var inserts = [req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
 
     return router;
 }();
